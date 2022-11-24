2a2e2a979cd9446227309d950960922cb247cc45
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2022-11-24 18:32:44 +0100

    added final url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 import Header from "../Components/Header";
 
-const REDIRECT_URL = "https://rubyexact.vercel.app/"
+const REDIRECT_URL = "https://rubyexact.vercel.app/oauth2/callback"
 
 const Home: NextPage = () => {
  const loginExact = () => {
