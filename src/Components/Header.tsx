77d5bab3ff9712e2b764b431dd5f889835b83e6e
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2022-11-24 20:55:22 +0100

    added twinfield api

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-function Header({loginExact}:any) {
+function Header({loginExact, handleLogout, accesToken}:any) {
 
   return (
     <div className='h-14 fixed backdrop-blur-sm w-screen p-2 items-center flex justify-between'>
         <div></div>
         <Link href="/"> <Image className='ml-32 cursor-pointer' src="https://i.imgur.com/S67aq4a.png" alt='ruby_logo' height={100} width={80}/></Link>
-        <button
+        {accesToken ? <button
+        className="mr-2 h-9 rounded-full bg-white/10 px-10 font-semibold text-white no-underline transition hover:bg-white/20"
+       onClick={handleLogout}
+      >
+      Sign out
+      </button> : <button
         className="mr-2 h-9 rounded-full bg-white/10 px-10 font-semibold text-white no-underline transition hover:bg-white/20"
        onClick={loginExact}
       >
       Sign in
-      </button>
+      </button>}
         </div>
   )
 }
