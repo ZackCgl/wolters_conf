8b6691e159815336815831e7bb49e419a6f1f77a
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2022-11-24 18:32:13 +0100

    added url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 import Header from "../Components/Header";
 
-const REDIRECT_URL = "https://web.postman.co/oauth2/callback"
+const REDIRECT_URL = "https://rubyexact.vercel.app/"
 
 const Home: NextPage = () => {
  const loginExact = () => {
