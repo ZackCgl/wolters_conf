8b1ab589a2a5f989c45dc8ed1415baca4e8e64ee
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2022-11-30 01:27:35 +0100

    added sidebar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 function Header({loginExact, handleLogout, accesToken}:any) {
+const [toggle, setToggle] = useState(false)
+
+const handleMobile = () => {
+  setToggle(!toggle)
+}
 
   return (
     
@@ -14,14 +19,14 @@ function Header({loginExact, handleLogout, accesToken}:any) {
     <Link href="/"> <Image className='cursor-pointer mt-4' src="https://i.imgur.com/S67aq4a.png" alt='ruby_logo' height={100} width={80}/></Link>
        
     
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <button onClick={handleMobile} type="button" className=" inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
       <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
+    {toggle && <div className=" w-full md:block md:w-auto" id="navbar-default">
+      <ul className=" flex flex-col p-4 mt-4 border bg-black border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
         <li>
-          <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+          <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded md:bg-transparent  md:p-0 dark:text-white" aria-current="page">Home</a>
         </li>
         <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
@@ -57,7 +62,7 @@ function Header({loginExact, handleLogout, accesToken}:any) {
           <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
       </ul>
-    </div>
+    </div>}
     
   </div>
 </nav>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -101,24 +101,24 @@ const handleLogout = () => {
         {!accesToken && <div className=" container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-           <Link
+           <div
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+             
             >
-              <h3 className="text-2xl font-bold">Sign Out →</h3>
+              <h3 onClick={handleLogin} className="text-2xl font-bold">Sign In →</h3>
               <div className="text-lg">
                 Sign in from your account - We offer the greatest features available.
               </div>
-            </Link>
-          <Link
+            </div>
+          <div
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+            
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>
               <div className="text-lg">
                 Learn more about Ruby Finance, our docs are open source.
               </div>
-            </Link>
+            </div>
           </div>
         </div>}
         
