60be89df988801877ce3b7e4e1ab4b9f854349aa
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2022-11-30 17:41:01 +0100

    add icons

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {FiSettings} from "react-icons/fi"
+import {AiOutlineQuestionCircle} from "react-icons/ai"
 
 function Sidebar() {
   return (
-    <div className='w-10 text-white font-bold '>
-        <p className='fixed bottom-9 left-4 cursor-pointer'>FAQ</p>
-        <p className='fixed bottom-4 left-4 cursor-pointer'>Settings</p>
+    <div className='w-10 text-white font-bold'>
+         <AiOutlineQuestionCircle className='fixed h-7 w-7 bottom-14 left-4 cursor-pointer'/>
+        <FiSettings className='fixed h-7 w-7 bottom-4 left-4 cursor-pointer'/>
     </div>
   )
 }
